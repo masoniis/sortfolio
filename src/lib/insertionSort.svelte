bb54cb5a1d7a9c1b4bda948923afda4683dbbfd9
masoniis <bott.m@comcast.net>
2023-06-29 21:17:58 -0600

    Last minor changes of the night

--- src/lib/insertionSort.svelte
+++ src/lib/insertionSort.svelte
@@ -34,32 +34,4 @@
 			await wait(150);
 		}
 	}
-	/* export async function insertionSort(store: Writable<CharObj[]>) {
-		// get the length of the array
-		let n = get(store).length;
-		// loop over the array from index 1 to the end
-		for (let i = 1; i < n; i++) {
-			// save the current element as key
-			let key = get(store)[i];
-			// initialize j as i - 1
-			let j = i - 1;
-			// compare key with the elements to its left
-			while (j >= 0 && get(store)[j].index > key.index) {
-				// swap the element at j with the element at j + 1
-				store.update((arr) => {
-					arr[j + 1] = arr[j];
-					return arr;
-				});
-				// decrement j by 1
-				j--;
-				// wait for 150 milliseconds
-				await wait(150);
-			}
-			// insert key at the correct position
-			store.update((arr) => {
-				arr[j + 1] = key;
-				return arr;
-			});
-		}
-	} */
 </script>
