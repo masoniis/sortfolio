ff72721d0a87a73d4a05efd06c16cd3e9d464418
masoniis <bott.m@comcast.net>
2024-04-06 12:35:27 -0600

    attempt 3

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,7 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-// /** @type {import('@sveltejs/kit').Config} */
-/** @type {import('@sveltejs/adapter-vercel').Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
