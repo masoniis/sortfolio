a6350533e38ea7714531982c6acbfcf12b56c84c
masoniis <bott.m@comcast.net>
2024-04-16 08:27:32 -0600

    seo optimization and vercel analytics

---
+++ src/routes/robots.txt/+server.ts
@@ -0,0 +1,18 @@
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }: { url: URL }) {
+	return new Response(
+		`User-agent: *
+Allow: /
+
+# Google adsbot ignores robots.txt unless specifically named!
+User-agent: AdsBot-Google
+Allow: /
+
+
+User-agent: GPTBot
+Disallow: /
+
+Sitemap: ${url.origin}/sitemap.xml
+`.trim()
+	);
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { inject } from '@vercel/analytics'
 
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 const config = {
