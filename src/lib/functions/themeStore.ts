92c2fd7f31121b9ebe4b9d2450523ba5c4545303
masoniis <bott.m@comcast.net>
2024-04-24 07:45:56 -0600

    added todo

--- src/lib/functions/themeStore.ts
+++ src/lib/functions/themeStore.ts
@@ -1,6 +1,8 @@
 import { type Writable, writable } from "svelte/store";
 import { browser } from '$app/environment';
 
+// TODO: Make theme automatically change background and other colors to have high contrast with the selected color. This may also lead to light mode being naturally implemented.
+
 const stored = browser ? window.localStorage.colorStore : "#22C55D";
 
 export var colorStore: Writable<string> = writable<string>(stored);
