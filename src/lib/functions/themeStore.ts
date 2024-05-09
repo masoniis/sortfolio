008b9e919454aa9d08e063c7a15f8ed9a972d55b
masoniis <masonmbott@gmail.com>
2024-05-09 12:14:35 -0600

    better adaptible theme including light mode through contrast support

--- src/lib/functions/themeStore.ts
+++ src/lib/functions/themeStore.ts
@@ -1,15 +1,82 @@
 import { type Writable, writable } from "svelte/store";
-import { browser } from '$app/environment';
+import { browser } from "$app/environment";
 
-// TODO: Make theme automatically change background and other colors to have high contrast with the selected color. This may also lead to light mode being naturally implemented.
+const accentBG = browser ? window.localStorage.colorStore : "#22C55D";
+const primaryBGDark = "#18181b";
+const primaryBGLight = "#d3d3db";
 
-const stored = browser ? window.localStorage.colorStore : "#22C55D";
-
-export var colorStore: Writable<string> = writable<string>(stored);
+export var colorStore: Writable<string> = writable<string>(accentBG);
 
 colorStore.subscribe((value) => {
-	if (browser) {
-		window.localStorage.setItem('colorStore', value);
-	}
+  if (browser) {
+    window.localStorage.setItem("colorStore", value);
+  }
+});
+
+// INFO: Functions to modify theme
+
+export function updateTheme(value: string) {
+  updateCSSVariable("--color-primaryaccentbg", value);
+
+  const backgroundColor = getContrastingColor(value);
+  updateCSSVariable("--color-primarybg", backgroundColor);
+  updateCSSVariable("--color-primaryaccentfg", backgroundColor); // color is same as BG usually
+
+  const foregroundColor = getContrastingColor(backgroundColor);
+  updateCSSVariable("--color-primaryfg", foregroundColor);
 }
-);
+
+function updateCSSVariable(name: string, value: string) {
+  const root = document.documentElement;
+  value = value ? value.replace("#", "") : "#22C55D";
+
+  const rgb = hexToWeirdRGB(value);
+
+  root.style.setProperty(name, rgb);
+}
+
+function calculateContrast(value1: string, value2: string) {
+  const color1 = value1.match(/[0-9a-fA-F]{2}/g)!.map((x) => parseInt(x, 16));
+  const color2 = value2.match(/[0-9a-fA-F]{2}/g)!.map((x) => parseInt(x, 16));
+
+  const l1 = 0.2126 * color1[0] + 0.7152 * color1[1] + 0.0722 * color1[2];
+  const l2 = 0.2126 * color2[0] + 0.7152 * color2[1] + 0.0722 * color2[2];
+
+  return Math.abs(l1 - l2) / 255;
+}
+
+function getContrastingColor(accentColor: string) {
+  const luminance = calculateContrast(accentColor, primaryBGDark);
+
+  if (luminance > 0.5) return primaryBGDark; // Keep dark bg
+  return primaryBGLight; // Switch to light bg
+}
+
+function weirdRGBtoHex(rgb: string) {
+  const values = rgb.split(" ");
+  return "#" + values.map((x) => parseInt(x).toString(16)).join("");
+}
+
+function multiplyColor(color: string, factor: number) {
+  const hexColor = color.replace("#", "");
+
+  const values = hexColor.match(/.{2}/g)!.map((hex) => parseInt(hex, 16));
+
+  const newValues = values.map((value) =>
+    Math.ceil(Math.min(255, value / factor)),
+  );
+
+  const newHexColor = newValues
+    .map((value) => value.toString(16).padStart(2, "0"))
+    .join("");
+
+  return "#" + newHexColor;
+}
+
+function hexToWeirdRGB(hex: string) {
+  const rgb = hex
+    .match(/.{1,2}/g)!
+    .map((x) => parseInt(x, 16))
+    .join(" ");
+  return rgb;
+}
--- src/routes/root/components/Theme.svelte
+++ src/routes/root/components/Theme.svelte
@@ -1,30 +1,18 @@
 <script lang="ts">
 	import { afterUpdate } from "svelte";
 	import { get } from "svelte/store";
-	import { colorStore } from "$lib/functions/themeStore";
+	import { colorStore, updateTheme } from "$lib/functions/themeStore";
 
 	let color: string = get(colorStore) || "#22C55D";
 
-	function updateCSSVariable(name: string, value: string) {
-		const root = document.documentElement;
-		value = value ? value.replace("#", "") : "#22C55D";
-		//
-		const rgb = value
-			.match(/.{1,2}/g)!
-			.map((x) => parseInt(x, 16))
-			.join(" ");
-
-		root.style.setProperty(name, rgb);
-	}
-
 	afterUpdate(() => {
-		updateCSSVariable("--color-primaryaccentbg", color);
+		updateTheme(color);
 		colorStore.set(color);
 	});
 
 	function reset() {
 		color = "#22C55D";
-		updateCSSVariable("--color-primaryaccentbg", color);
+		updateTheme(color);
 		colorStore.set(color);
 	}
 </script>
